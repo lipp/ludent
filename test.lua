8ea1f492568537dbf9eccbe6ccfb8eba4a0a7784
Gerhard Lipp <gelipp@gmail.com>
2013-02-13 16:01:05 +0100

    first draft, working quite good
    no options no doc no help yet

---
+++ test.lua
@@ -0,0 +1,59 @@
+local a = function(a,b)
+   print(
+      1,2
+	)
+end
+
+local q = function() end
+
+on_message(function(blabla)
+	      local s = {
+		 1,
+		 2,
+	      }
+	      local q = {123,45}
+	   end,
+	   789)
+
+
+on_message(function(
+		 a,
+		 b,
+		 c
+		   )
+	      local s = {
+		 1,
+		 2,
+	      }
+	      local q = {123,45}
+	   end)
+
+on_bla(
+   function()     
+      local p = 123
+   end,1,2,3)
+
+on_bla(function()     
+	  local p = 123
+   end,1,2,3)
+
+
+on_bla(function()     
+   end)
+
+
+if bla then
+   if deeper then
+      print(123)
+   end
+   a = {
+      1,2,3
+   }
+elseif ww then
+   a = function() end
+else
+   print(
+      1,2,
+      3
+	)
+end
