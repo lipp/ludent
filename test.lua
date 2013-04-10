8bc5db43288fdbdfc270dd6b54caddf76d64c69b
Gerhard Lipp <gelipp@googlemail.com>
2013-04-10 09:41:03 +0200

    add post match for one line functions in arrays

--- test.lua
+++ test.lua
@@ -1,63 +1,74 @@
 local a = function(a,b)
-   print(
-      1,2
-	)
+  print(
+    1,2
+  )
 end
 
 local q = function() end
 
 local ok,bla = pcall(
-function()
-		     end)
+  function()
+  end)
+
+local x = {
+  1,
+  2,
+}
+
+spy.new(function() print() end)
+
+local t = {
+  spy.new(function() end),
+}
 
 on_message(function(blabla)
-	      local s = {
-		 1,
-		 2,
-	      }
-	      local q = {123,45}
-	   end,
-	   789)
+    local s = {
+      1,
+      2,
+    }
+    local q = {123,45}
+  end,
+789)
 
 
 on_message(function(
-		 a,
-		 b,
-		 c
-		   )
-	      local s = {
-		 1,
-		 2,
-	      }
-	      local q = {123,45}
-	   end)
+      a,
+      b,
+      c
+    )
+    local s = {
+      1,
+      2,
+    }
+    local q = {123,45}
+  end)
 
 on_bla(
-   function()     
-      local p = 123
-   end,1,2,3)
+  function()
+    local p = 123
+  end,1,2,3)
 
-on_bla(function()     
-	  local p = 123
-   end,1,2,3)
+on_bla(function()
+    local p = 123
+  end,1,2,3)
 
 
-on_bla(function()     
-   end)
+on_bla(function()
+  end)
 
 
 if bla then
-   if deeper then
-      print(123)
-   end
-   a = {
-      1,2,3
-   }
+  if deeper then
+    print(123)
+  end
+  a = {
+    1,2,3
+  }
 elseif ww then
-   a = function() end
+  a = function() end
 else
-   print(
-      1,2,
-      3
-	)
+  print(
+    1,2,
+    3
+  )
 end
